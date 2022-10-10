90c3c0a0eb087c3c9967a902f4d274f7e96740bf
Sarah Lozier <sarah.lozier@gmail.com>
2022-10-10 19:17:29 -0400

    trying to get add student to work

---
+++ src/components/instructor/AddStudent.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addStudent } from '../../store/reducers/instructorSlice';
+import Popup from 'reactjs-popup';
+
+const AddStudent = () => {
+    const params = useParams();
+
+    const [ form, setForm ] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+    });
+
+    const dispatch = useDispatch();
+
+    const handleChange = prop => event => {
+        setForm({
+            ...form,
+            [prop]: event.target.value
+        })
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(addStudent({
+        firstName: form.firstName,
+        lastName: form.lastName,
+        email: form.email,
+        username: form.username,
+        password: form.password,
+        }, params.id));
+    };
+
+    useEffect(() => {
+    }, [form]);
+
+    console.log('ADD STUDENT COMP', form.email);
+
+    return(
+        <><Popup trigger={<button>Add Student</button>} position="bottom left">
+        {close => (
+            <div>
+          <form id='form' onSubmit={handleSubmit}>
+               <h3>Edit Student</h3>
+               <label htmlFor="firstName">First Name:</label>
+               <input name='firstName' value={form.firstName} onChange={handleChange('firstName')}/>
+               <label htmlFor="lastName">Last Name:</label>
+               <input name='lastName' value={form.lastName} onChange={handleChange('lastName')}/>
+               <label htmlFor="email">Email:</label>
+               <input name="email" value={form.email} onChange={handleChange('email')}/>
+               <label htmlFor="username">Username:</label>
+               <input name='username' value={form.username} onChange={handleChange('username')}/>
+               <label htmlFor="password">Password:</label>
+               <input name="password" value={form.password} onChange={handleChange('password')}/>
+               <button type="submit">Submit Changes</button>
+               <Link to={`/instructorPortal/${params.id}/students`}>Cancel</Link>
+              </form> 
+          <a className="close" onClick={close}>
+            &times;
+          </a>
+          </div>
+        )}
+    </Popup>
+        </>
+    )
+}
+
+export default AddStudent;
