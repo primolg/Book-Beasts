33cd69cb946d1cfffc49a6b69211fe64a5770a9e
primogill <primoledeboergill@gmail.com>
2022-10-10 21:58:34 +0200

    temporary commented out auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import InstructorPortal from "./components/instructor/InstructorPortal";
 import InstructorStudents from "./components/instructor/InstructorStudents";
