215a9efe6d3f3d8443b3856bd3bda9acc6b804a6
sllozier <sarah.lozier@gmail.com>
2022-10-19 15:40:10 -0400

    merg fix?

--- src/components/instructor/instructorBooks/index.js
+++ src/components/instructor/instructorBooks/index.js
@@ -1,11 +1,10 @@
 import BookRow from "./BookRow";
 import BookTable from "./BookTable";
 import ViewSingleBook from "./ViewSingleBook";
-//import SinglePage from "./SinglePage";
+
 
 export {
     BookRow,
     BookTable,
     ViewSingleBook,
-    //SinglePage,
 }
