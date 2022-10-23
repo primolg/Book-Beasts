345e317034e3ffd44aa1c901ec88785c0af1adb5
Brandon Lowe <BLoweCLE@gmail.com>
2022-10-23 14:04:12 -0500

    added switch statement to render different page templates depending on templateId on each page

---
+++ src/components/books/BookView1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BookView1 = React.forwardRef(({page}, ref) => {
+    return ( page ? (
+        <div ref={ref} className="demoPage view-page-outer-div bookview3-outer-div">
+                <div className="view-small-image-page">
+                    {page.page.image ? (<img className="view-page-image" src={page.page.image}/>) : <div className="filler-image"></div>}
+                </div>
+                <div className="view-small-text-page" id="view-pageText">
+                    <p>{page.page.content}</p>
+                </div>
+        </div>
+        ) : (<div className="demoPage filler-page"></div>)
+    );
+});
+
+export default BookView1;
